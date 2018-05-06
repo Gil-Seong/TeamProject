2d5a34b15fd676e190911d50f1b6fff297a12e0a
user <user@DESKTOP-50L7JUS>
2018-05-06 18:10:15 +0900

    [환경설정] TEST DB Connection

---
+++ project/src/main/java/mapper/DBConnectionTest.java
@@ -0,0 +1,14 @@
+package mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import vo.DBConnectionVo;
+
+@Mapper
+public class DBConnectionTest {
+
+//	public List list(DBConnectionVo vo) throws Exception;
+	
+}
---
+++ project/src/main/java/vo/DBConnectionVo.java
@@ -0,0 +1,30 @@
+package vo;
+
+public class DBConnectionVo {
+	
+	private int bno;
+	private String userName;
+	private String contents;
+	
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	
+	
+
+}
