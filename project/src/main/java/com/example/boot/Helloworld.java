87c2b1f7f5196ccdd0a5afcbeb6a6c30ad9bf881
user <user@DESKTOP-50L7JUS>
2018-05-13 13:43:55 +0900

    [소스수정] spring boot 로그 백 설정

--- project/src/main/java/com/example/boot/Helloworld.java
+++ project/src/main/java/com/example/boot/Helloworld.java
@@ -1,15 +1,25 @@
 package com.example.boot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Helloworld {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@RequestMapping("/")
-	public String hello() {
+	@RequestMapping(value = "/help" ,method = RequestMethod.GET )
+	public String help() {
 		
-		return "hello world";
+		logger.debug("logback example - debug level");
+		logger.info("logback example - info level");
+		logger.warn("logback example - warn level");
+		logger.error("logback example - error level");
+		
+		return "hello page";
 	}
 	
 }
